3e332d8229a673f2571564d540b45af09a3690ee
RuiOkazaki <okazaki200226@gmail.com>
2022-03-11 18:32:27 +0900

    `add`: interseptorを自作した

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module, ClassSerializerInterceptor } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomInterseptor } from './custom.interseptor';
 
 @Module({
   imports: [],
@@ -10,7 +11,8 @@ import { AppService } from './app.service';
     AppService,
     {
       provide: APP_INTERCEPTOR,
-      useClass: ClassSerializerInterceptor,
+      // useClass: ClassSerializerInterceptor,
+      useClass: CustomInterseptor,
     },
   ],
 })
---
+++ src/custom.interseptor.ts
@@ -0,0 +1,24 @@
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { map } from 'rxjs';
+
+export class CustomInterseptor implements NestInterceptor {
+  intercept(context: ExecutionContext, handler: CallHandler) {
+    console.log('before');
+    console.log({ context });
+
+    return handler.handle().pipe(
+      map((data) => {
+        const response = {
+          ...data,
+          createdAt: data.created_at,
+        };
+        delete response.created_at;
+        delete response.updated_at;
+        console.log('mapping');
+        console.log({ data });
+
+        return response;
+      }),
+    );
+  }
+}
